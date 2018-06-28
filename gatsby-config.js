22f3634f3f6f4c3cf2d418f164cb68e46c55bdba
Tyler <tyler.hahn@gogeddit.com>
2018-06-28 13:32:05 -0500

    netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,4 +3,6 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [`gatsby-plugin-sass`],
 }
